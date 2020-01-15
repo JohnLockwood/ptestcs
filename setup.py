8da95699186b47d526e6e7319375cc74af95569c
John Lockwood <JohnLockwood2999@gmail.com>
2020-01-14 20:35:26 -0500

    Rebuild, rename, re-upload.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-package-test-codesolid',
+    name='package_test_codesolid',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
